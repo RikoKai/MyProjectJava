fc2e85835fc69a4db5d535a29a663e745cbf67fe
RikoKai <48921348+RikoKai@users.noreply.github.com>
2021-11-03 18:32:42 +0600

    main class dopolnen

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner sc=new Scanner(System.in);
         Student student[]=new Student[3];
        student[0]=new Student(1,"Muh A","2002","Ast","FIT",2,"Sib-21");
         student[1]=new Student(2,"Muh NUr","2001","Alt","FEN",1,"Sib-21");
         student[2]=new Student(2,"Muh Aslan","2003","Atyrau","FIT",3,"IS31");
 
+        System.out.println("KErek facultet korset");
+        String f=sc.nextLine();
         for (int i = 0; i <student.length ; i++) {
+            if(student[i].getFacultet().equalsIgnoreCase(f))
             System.out.println(student[i].toString());
         }
 
